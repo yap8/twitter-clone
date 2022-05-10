9d1206849fe9a6c4d518112f61d61c727809a9a4
yap8 <nebozhenko.arseny@gmail.com>
2022-05-10 12:14:18 +0300

    added cors and json middleware

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const sequelize = require('./db');
 const models = require('./models');
+const cors = require('cors');
 
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // start
 const start = async () => {
