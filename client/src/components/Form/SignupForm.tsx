f535c63d3b6c1a9518f3c34e078724e6db6a497e
yap8 <nebozhenko.arseny@gmail.com>
2022-05-10 19:11:29 +0300

    moved SignupForm into its component

---
+++ client/src/components/Form/SignupForm.tsx
@@ -0,0 +1,60 @@
+import CloseIcon from '@mui/icons-material/Close';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Button, IconButton, TextField } from '@mui/material';
+
+interface SignupFormProps {
+  handleClose: () => any;
+}
+
+const SignupForm: React.FC<SignupFormProps> = ({ handleClose }) => {
+  return (
+    <form
+      className="absolute top-1/2 left-1/2 w-full max-w-lg bg-white border rounded-xl p-2"
+      style={{ transform: 'translate(-50%, -50%)' }}
+    >
+      <div className="flex">
+        <div className="flex-1">
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <TwitterIcon
+          className="flex-1 text-blue-400"
+          style={{ fontSize: '2rem' }}
+        />
+        <div className="flex-1"></div>
+      </div>
+      <div className="flex flex-col px-10">
+        <h1 className="text-3xl font-semibold mb-6">Create your account</h1>
+        <div className="mb-6">
+          <TextField className="w-full" label="Name" variant="outlined" />
+        </div>
+        <div className="mb-6">
+          <TextField
+            className="w-full"
+            name="email"
+            type="email"
+            label="Email"
+            variant="outlined"
+          />
+        </div>
+        <div className="mb-10">
+          <TextField
+            className="w-full"
+            name="name"
+            type="password"
+            label="Password"
+            variant="outlined"
+          />
+        </div>
+        <div className="pb-10">
+          <Button className="w-full" variant="outlined" size="large">
+            Create
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default SignupForm;
--- client/src/pages/Welcome/index.tsx
+++ client/src/pages/Welcome/index.tsx
@@ -1,15 +1,9 @@
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GoogleIcon from '@mui/icons-material/Google';
-import CloseIcon from '@mui/icons-material/Close';
-import {
-  Backdrop,
-  Button,
-  Fade,
-  IconButton,
-  Modal,
-  TextField,
-} from '@mui/material';
+import { Button, Fade, Modal } from '@mui/material';
 import { useState } from 'react';
+
+import SignupForm from '../../components/Form/SignupForm';
 
 const Welcome: React.FC = () => {
   const [open, setOpen] = useState(false);
@@ -19,64 +13,11 @@ const Welcome: React.FC = () => {
 
   return (
     <section className="flex h-screen">
-      <Modal
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
+      <Modal open={open} onClose={handleClose} closeAfterTransition>
         <Fade in={open}>
-          <form
-            className="absolute top-1/2 left-1/2 w-full max-w-lg bg-white border rounded-xl p-2"
-            style={{ transform: 'translate(-50%, -50%)' }}
-          >
-            <div className="flex">
-              <div className="flex-1">
-                <IconButton onClick={handleClose}>
-                  <CloseIcon />
-                </IconButton>
-              </div>
-              <TwitterIcon
-                className="flex-1 text-blue-400"
-                style={{ fontSize: '2rem' }}
-              />
-              <div className="flex-1"></div>
-            </div>
-            <div className="flex flex-col px-10">
-              <h1 className="text-3xl font-semibold mb-6">
-                Create your account
-              </h1>
-              <div className="mb-6">
-                <TextField className="w-full" label="Name" variant="outlined" />
-              </div>
-              <div className="mb-6">
-                <TextField
-                  className="w-full"
-                  name="email"
-                  type="email"
-                  label="Email"
-                  variant="outlined"
-                />
-              </div>
-              <div className="mb-10">
-                <TextField
-                  className="w-full"
-                  name="name"
-                  type="password"
-                  label="Password"
-                  variant="outlined"
-                />
-              </div>
-              <div className="pb-10">
-                <Button className="w-full" variant="outlined" size="large">
-                  Create
-                </Button>
-              </div>
-            </div>
-          </form>
+          <div>
+            <SignupForm handleClose={handleClose} />
+          </div>
         </Fade>
       </Modal>
       <div className="w-1/2 flex items-center justify-center overflow-hidden">
